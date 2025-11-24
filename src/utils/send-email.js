49d7ff1cc94bab1ca2fb55636f54545d331f943d
devizetechsolution <sam.an.vns6@gmail.com>
2025-11-24 20:30:41 +0530

    update: resetpassword

--- src/utils/send-email.js
+++ src/utils/send-email.js
@@ -5,9 +5,7 @@ dotenv.config();
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
