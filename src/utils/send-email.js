9f1446a941f4bf14a4ef4963a0a6a4eab822d173
Samad <sam.an.vns6@gmail.com>
2025-11-25 11:31:34 +0530

    update: email send-4

--- src/utils/send-email.js
+++ src/utils/send-email.js
@@ -5,9 +5,6 @@ dotenv.config();
 export const sendEmail = async ({ to, subject, text, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      smtp: "smtp.gmail.com",
-      host: '0.0.0.0:0',
-      secure: true,
       service: "gmail", 
       auth: {
         user: process.env.EMAIL_USER,
