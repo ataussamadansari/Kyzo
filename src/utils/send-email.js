0595ee889c69695a7364d58cf63d012fdb06d988
Samad <sam.an.vns6@gmail.com>
2025-11-25 11:13:00 +0530

    update: email send-2

--- src/utils/send-email.js
+++ src/utils/send-email.js
@@ -3,7 +3,10 @@ import nodemailer from "nodemailer";
 export const sendEmail = async ({ to, subject, text, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail", // Gmail (or use SMTP config)
+      smtp: "smtp.gmail.com",
+      host: 465,
+      secure: true,
+      service: "gmail", 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
