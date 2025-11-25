7a106b22f882cdf5ee501fc1eab7a72dac88b646
Samad <sam.an.vns6@gmail.com>
2025-11-25 16:50:37 +0530

    update: db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,46 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-  } catch (error) {
-    console.log("❌ MongoDB Error:", error.message);
-    process.exit(1);
+const MONGODB_URI = process.env.MONGO_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("⛔ Missing MONGODB_URI in environment variables!");
+}
+
+// Global is used here to maintain a cached connection across hot reloads in dev
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export default async function dbConnect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 5000, // Faster failure
+      maxPoolSize: 1
+    }).then((mongoose) => {
+      console.log("MongoDB Connected Successfully 🚀");
+      return mongoose;
+    });
   }
-};
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//   } catch (error) {
+//     console.log("❌ MongoDB Error:", error.message);
+//     process.exit(1);
+//   }
+// };
